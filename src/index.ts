f1746c798eb2ec49e221dbe071dc93d905ec2927
tbv125 <tbv125>
2022-12-01 14:10:08 +0300

    Default

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,27 @@
 import e from "express";
 import path from 'path';
-import { requestTime, logger } from './middlewares.js';
+import { logger } from './middlewares.js';
 import serverRoutes from './routes/servers.js'
 
 const __dirname = path.resolve();
 const app = e();
 const PORT = process.env.PORT ?? 3000;
 
-console.log(app.get('view engine'));
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, 'ejs'));
-console.log(app.get('views'));
+
+console.log('ViewEngine:', app.get('view engine'));
+console.log('Views:', app.get('views'));
 
 app.use(e.static(path.resolve(__dirname, 'static')))
 app.use(e.json())
 app.use(e.urlencoded({ extended: false }))
-app.use(requestTime);
 app.use(logger);
-
-app.use('/*', (req, res, next) => {
-    console.log('Request Type:', req.method)
-    next()
-})
 
 app.use(serverRoutes);
 
-app.get('/', (req, res) => {
-    res.render('index', { title: "Main Page", active: "main" });
-    console.log(path.resolve(__dirname, 'static'));
+app.get('/', (req, res) => {    
+    res.render('index', { title: "Main Page", active: "main" });        
 })
 
 app.get('/features', (req, res) => {
@@ -38,7 +32,7 @@ app.get('/download', (req, res) => {
     res.download(path.resolve(__dirname, 'ejs', 'index.ejs'));
 });
 
-//Запуск собак
+//Запуск сервера
 app.listen(PORT, () => {
     console.log(`Что - то запустилось на ${PORT}...`);
 });
